a9a470146a02e3184475dea2c0dd239ea452c449
minador3 <min.wei@outlook.fr>
2025-07-27 21:31:08 +0400

    sunday last commit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mwei <mwei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 16:58:46 by mwei              #+#    #+#             */
-/*   Updated: 2025/07/27 20:55:26 by mwei             ###   ########.fr       */
+/*   Updated: 2025/07/27 21:30:51 by mwei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n)
 	{
 		if (s1[i] != s2[i] || s1[i] == '\0' || s2[i] == '\0')
-			// return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-			return (s1[i] - s2[i]);
-
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
