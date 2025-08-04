b19bd7df0f2b1bd3d899aec9746dd375c193d3a5
minador3 <min.wei@outlook.fr>
2025-08-04 17:56:01 +0400

    bonus1

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mwei <mwei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:12:51 by mwei              #+#    #+#             */
-/*   Updated: 2025/08/04 17:55:26 by mwei             ###   ########.fr       */
+/*   Updated: 2025/08/04 17:55:54 by mwei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list last;
+	t_list	last;
 
 	if (!lst || !new)
 		return ;
